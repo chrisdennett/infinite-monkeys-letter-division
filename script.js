ef96c8122bbfa321f5dce519835ad5037dfb33f7
chrisdennett <chrisdennett@gmail.com>
2024-02-25 10:13:14 +0000

    Creating full set of grids

--- script.js
+++ script.js
@@ -3,10 +3,34 @@ const canvas = document.querySelector('#canvas');
 // Setup
 canvas.width = 1280;
 canvas.height = 720;
+const blockSize = 10;
+const gridWidth = blockSize * 8;
+const gridHeight = blockSize * 8;
+const padding = blockSize * 2;
+
+const gridsAcross = Math.floor(canvas.width / (gridWidth + padding));
+const gridsDown = Math.floor(canvas.height / (gridHeight + padding));
 
 const ctx = canvas.getContext('2d');
-const grid1 = createRandom8BitGrid();
-drawGrid(ctx, grid1, 20);
+
+setInterval(drawSetOfGrids, 500);
+
+function drawSetOfGrids(){
+  ctx.clearRect(0,0,canvas.width, canvas.height)
+
+  for(let i=0; i<gridsDown; i++){
+    for(let j=0; j<gridsAcross; j++){
+      const grid1 = createRandom8BitGrid();
+
+      ctx.save();
+      const gridX = j * (gridWidth + padding)
+      const gridY = i * (gridHeight + padding)
+      ctx.translate(gridX, gridY)
+      drawGrid(ctx, grid1, blockSize);
+      ctx.restore();
+    }
+  }
+}
 
 function createRandom8BitGrid() {
   const grid = [];
